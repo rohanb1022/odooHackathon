8247abadc14cff7e63b82eb27237259b048c5f92
Rohan Bhangale <154115899+rohanb1022@users.noreply.github.com>
2026-07-12 16:46:18 +0530

    Refactor date-fns imports in BookingsCalendar

--- Frontend/src/components/BookingsCalendar.tsx
+++ Frontend/src/components/BookingsCalendar.tsx
@@ -2,11 +2,8 @@
 
 import React from 'react';
 import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
-import format from 'date-fns/format';
-import parse from 'date-fns/parse';
-import startOfWeek from 'date-fns/startOfWeek';
-import getDay from 'date-fns/getDay';
-import enUS from 'date-fns/locale/en-US';
+import { format, parse, startOfWeek, getDay } from 'date-fns';
+import { enUS } from 'date-fns/locale';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 
 const locales = {
